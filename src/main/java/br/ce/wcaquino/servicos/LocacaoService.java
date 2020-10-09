1b8bcfeceb8b1bea9bfd28436151e522723abc36
João Monteiro <joao.monteiro@aws.bpp>
2020-10-09 16:23:09 -0300

    Mudança para garantir um dos princípios de teste. O principio de auto validação

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -36,8 +37,8 @@ public class LocacaoService {
 		//ação
 		Locacao locacao = service.alugarFilme(usuario,filme);
 		//resultado
-		System.out.println(locacao.getDataLocacao());
-		System.out.println(locacao.getDataRetorno());
-		System.out.println(locacao.getValor());
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		System.out.println(locacao.getValor() == 5.0);
 	}
 }
