cda48d776b98e67ee1305828821d2ce64f06f0d1
jgmonteiro1 <jgmoreira15@gmail.com>
2020-10-28 17:58:07 -0300

    Utilizando TDD. Demais testes criados seguidos por seus respectivos métodos

--- src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -4,4 +4,16 @@ public class Calculadora {
     public int somar(int a, int b) {
         return a+b;
     }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
 }
--- src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -17,4 +17,40 @@ public class CalculadoraTest {
         Assert.assertEquals(8, resultado);
     }
 
+    @Test
+    public void deveSubtrairDoisValores(){
+        //cenario
+        int a = 5;
+        int b = 3;
+        Calculadora calc = new Calculadora();
+        //acao
+        int resultado = calc.subtrair(a,b);
+        //resultado
+        Assert.assertEquals(2, resultado);
+    }
+
+    @Test
+    public void deveMultiplicarDoisValores(){
+        //cenario
+        int a = 5;
+        int b = 3;
+        Calculadora calc = new Calculadora();
+        //acao
+        int resultado = calc.multiplicar(a,b);
+        //resultado
+        Assert.assertEquals(15, resultado);
+    }
+
+    @Test
+    public void deveDividirDoisValores(){
+        //cenario
+        int a = 10;
+        int b = 2;
+        Calculadora calc = new Calculadora();
+        //acao
+        int resultado = calc.divide(a,b);
+        //resultado
+        Assert.assertEquals(5, resultado);
+    }
+
 }
