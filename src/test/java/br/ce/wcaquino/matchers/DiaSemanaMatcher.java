237d91d7c7fffcaf2b852ca876d08c2397148ff2
jgmonteiro1 <jgmoreira15@gmail.com>
2020-10-29 14:50:12 -0300

    Deixando a mensagem de erro autoexplicativa

--- src/test/java/br/ce/wcaquino/matchers/DiaSemanaMatcher.java
+++ src/test/java/br/ce/wcaquino/matchers/DiaSemanaMatcher.java
@@ -4,7 +4,9 @@ import br.ce.wcaquino.utils.DataUtils;
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class DiaSemanaMatcher extends TypeSafeMatcher<Date> {
 
@@ -20,6 +22,9 @@ public class DiaSemanaMatcher extends TypeSafeMatcher<Date> {
     }
 
     public void describeTo(Description description) {
-
+        Calendar data = Calendar.getInstance();
+        data.set(Calendar.DAY_OF_WEEK, diaSemana);
+        String dataExtenso = data.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, new Locale("pt", "BR"));
+        description.appendText(dataExtenso);
     }
 }
